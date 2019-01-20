b16adc2d92091aced7fef4b3877f6d5baa07358f
Shreyas kulkarni <Shreyasrameshkulkarni@gmail.com>
2019-01-20 14:00:15 +0530

    Live facedetection blur instantiated

--- main.py
+++ main.py
@@ -28,21 +28,29 @@ while 1:
     # Detects faces of different sizes in the input image
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+    result_image = img.copy()
     for (x, y, w, h) in faces:
         # To draw a rectangle in a face
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = img[y:y + h, x:x + w]
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 1)
+        sub_face = img[y:y + h, x:x + w]
+        # roi_gray = gray[y:y + h, x:x + w]
+        # roi_color = img[y:y + h, x:x + w]
 
         # Detects eyes of different sizes in the input image
-        eyes = eye_cascade.detectMultiScale(roi_gray)
+        # eyes = eye_cascade.detectMultiScale(roi_gray)
 
         # To draw a rectangle in eyes
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 127, 255), 2)
+        # for (ex, ey, ew, eh) in eyes:
+        #    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 127, 255), 2)
 
-    # Display an image in a window
-    cv2.imshow('img', img)
+        # Avging a blur
+        avging = cv2.blur(sub_face, (10, 10))
+
+        result_image[y:y + h, x:x + w] = avging
+        # Display an image in a window
+        cv2.imshow('img', img)
+
+        cv2.imshow('averaging',result_image)
 
     # Wait for Esc key to stop
     k = cv2.waitKey(30) & 0xff
